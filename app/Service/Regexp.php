1de7f4d464699995e566bfa4883f3896eac7c896
Bastien Nivet <bastien.nivet@mobisysteme.com>
2014-10-04 01:08:01 +0200

    add case of reversed key/value in foreach

--- app/Service/Regexp.php
+++ app/Service/Regexp.php
@@ -33,6 +33,12 @@ class Regexp
     const FOREACH_WITH_KEY_SYNTAXE_PHP      = '<?php foreach($$1 as $$2 => $$3): ?>';
 
     /**
+     * Foreach (key=>value) REVERSE
+     */
+    const FOREACH_WITH_KEY_REVERSE_SYNTAXE_TEMPLATE = '/<foreach\s+var="([a-z0-9_]+)"\s+as="([a-z0-9_]+)"\s+key="([a-z0-9_]+)">/';
+    const FOREACH_WITH_KEY_REVERSE_SYNTAXE_PHP      = '<?php foreach($$1 as $$3 => $$2): ?>';
+
+    /**
      * Foreach eot
      */
     const FOREACH_CLOSING_SYNTAXE_TEMPLATE = '/<\/foreach>/';
@@ -73,6 +79,7 @@ class Regexp
             self::PRINT_SYNTAXE_TEMPLATE,
             self::FOREACH_SYNTAXE_TEMPLATE,
             self::FOREACH_WITH_KEY_SYNTAXE_TEMPLATE,
+            self::FOREACH_WITH_KEY_REVERSE_SYNTAXE_TEMPLATE,
             self::FOREACH_CLOSING_SYNTAXE_TEMPLATE,
             self::IF_SYNTAXE_TEMPLATE,
             self::ELSEIF_SYNTAXE_TEMPLATE,
@@ -92,6 +99,7 @@ class Regexp
             self::PRINT_SYNTAXE_PHP,
             self::FOREACH_SYNTAXE_PHP,
             self::FOREACH_WITH_KEY_SYNTAXE_PHP,
+            self::FOREACH_WITH_KEY_REVERSE_SYNTAXE_PHP,
             self::FOREACH_CLOSING_SYNTAXE_PHP,
             self::IF_SYNTAXE_PHP,
             self::ELSEIF_SYNTAXE_PHP,
